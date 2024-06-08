8193f64371490307e8ff75ab8959dff4db5d8233
Mosayeb09 <mosayebhossain09@gmail.com>
2024-06-08 17:40:34 +0600

    adding about section

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -2,8 +2,19 @@
 
 const About = () => {
     return (
-        <div>
-            
+        <div name='about' className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full  ">
+                <div className="pb-8 ">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">About</p>
+                </div>
+                <p className="text-xl mt-20  ">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam molestiae cumque sit quasi voluptas velit qui itaque possimus, maxime commodi voluptatibus nesciunt magnam magni distinctio maiores doloribus soluta inventore officiis.
+                </p>
+                <br />
+                <p className="text-xl">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, tenetur! Cupiditate totam illum ipsum, est rem dolores iure dolorem similique optio, odit voluptatum tenetur odio praesentium voluptate? Beatae nobis minima ullam consectetur eligendi? Magnam doloremque quos voluptatum, quod quas nulla laborum rem dolor tenetur iusto blanditiis illum, nostrum aliquam nemo!
+                </p>
+            </div>
         </div>
     );
 };
