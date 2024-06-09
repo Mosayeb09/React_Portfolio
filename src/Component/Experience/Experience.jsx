b9256932889bbfa6cf155b9740e94f3b4a8a622b
Mosayeb09 <mosayebhossain09@gmail.com>
2024-06-09 20:05:51 +0600

    adding smoot scrol

--- src/Component/Experience/Experience.jsx
+++ src/Component/Experience/Experience.jsx
@@ -61,7 +61,7 @@ const Experience = () => {
         }
     ]
     return (
-        <div name='Experience' className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
+        <div  name='experience' className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
             <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white   '>
                 <div>
                     <p className='text-4xl font-bold inline border-b-4 p-2 border-gray-500' >Experience</p>
