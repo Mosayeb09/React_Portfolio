3064f5833497567a8e79de8fd5c4c633292f64c6
Mosayeb09 <mosayebhossain09@gmail.com>
2024-10-20 18:45:28 +0600

    adding code link to protfolio sction

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -8,20 +8,28 @@ const Portfolio = () => {
 
     const portfolio = [
         {
-            id:1,
-            src:Food_can_go
+            id: 1,
+            src: Food_can_go,
+            demo: 'https://food-can-go-frontend.vercel.app/',
+            code: 'https://github.com/Mosayeb09/FOOD_CAN_GO'
         },
         {
-            id:2,
-            src:Edusity
+            id: 2,
+            src: Edusity,
+            demo: 'https://edusity-sepia.vercel.app/',
+            code: 'https://github.com/Mosayeb09/Edusity.git'
         },
         {
-            id:3,
-            src:N_X
+            id: 3,
+            src: N_X,
+            demo: 'https://mosayeb09.github.io/N_X/',
+            code: 'https://github.com/Mosayeb09/N_X.git'
         },
         {
-            id:4,
-            src:Doctors
+            id: 4,
+            src: Doctors,
+            demo: 'https://khanbiotech.com/',
+            code: 'home'
         },
     ]
 
@@ -34,18 +42,25 @@ const Portfolio = () => {
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
                     <p className=' pt-6'>Check out some of my work right here</p>
                 </div>
-                
+
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-                {
-                    portfolio.map(({id,src})=>( <div key={id}className='shadow-md shadow-gray-600 rounded-lg '>
-                        <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
-                        <div className='flex items-center justify-center '>
-                            <button className=' w-1/2 px6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                            <button className='w-1/2 px6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
-                        </div>
-                    </div>))
-                }
-                   
+                    {
+                        portfolio.map(({ id, src,demo,code }) => (<div key={id} className='shadow-md shadow-gray-600 rounded-lg '>
+                            <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
+                            <div className='flex items-center justify-center '>
+                                {/* <button className=' w-1/2 px6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                            <button className='w-1/2 px6 py-3 m-4 duration-200 hover:scale-105'>Code</button> */}
+
+                                <a href={demo} target="_blank" rel="noopener noreferrer" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
+                                    Demo
+                                </a>
+                                <a href={code} target="_blank" rel="noopener noreferrer" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
+                                    Code
+                                </a>
+                            </div>
+                        </div>))
+                    }
+
                 </div>
             </div>
 
